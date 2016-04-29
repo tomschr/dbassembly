50d256b6f85d00787d06c5b4269aa4ec9864f3a0
Thomas Schraitle <tom_schr@web.de>
2016-04-29 22:38:42 +0200

    Add GPL license

--- src/dbassembly/__main__.py
+++ src/dbassembly/__main__.py
@@ -1,14 +1,32 @@
+#
+# Copyright (c) 2016 SUSE Linux GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
 """
-Entrypoint module, in case you use `python -mdbassembly`.
+Entrypoint module, in case you use :command:`python -mdbassembly`.
 
+Why does this file exist, and why :func:`__main__`?
+For more info, read:
 
-Why does this file exist, and why __main__? For more info, read:
-
-- https://www.python.org/dev/peps/pep-0338/
-- https://docs.python.org/2/using/cmdline.html#cmdoption-m
-- https://docs.python.org/3/using/cmdline.html#cmdoption-m
+* https://www.python.org/dev/peps/pep-0338/
+* https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
+
 from dbassembly.cli import main
 
 if __name__ == "__main__": # noqa
-    main()
+    main()  # noqa
