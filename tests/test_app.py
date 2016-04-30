5f8fb8c4d4396dd9afe2572365568176e0d949de
Thomas Schraitle <tom_schr@web.de>
2016-04-30 15:30:25 +0200

    New tests for dbassembly.app.py

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+
+from dbassembly.app import App
+from unittest.mock import patch
+# from mock import patch
+
+def test_app():
+    app = App(None)
+    assert repr(app)
+
+@patch('dbassembly.app.App.process')
+def test_app_process(mock_app):
+    app = App({'<assembly>':'a', '<output>':'b'})
+    result = app.process()
+    mock_app.assert_called_once('a', 'b')
