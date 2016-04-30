908609585ba71927777f225d3da0d063cba9423f
Thomas Schraitle <tom_schr@web.de>
2016-04-30 13:02:42 +0200

    Add more tests for assembly and output

--- tests/test_dbassembly.py
+++ tests/test_dbassembly.py
@@ -1,6 +1,19 @@
 
-from dbassembly.cli import main
+from dbassembly.cli import main, parsecli
+from .tools import raises
 
-
+@raises(SystemExit)
 def test_main():
     main([])
+
+def test_main_with_assembly_file():
+    assembly = 'assembly.xml'
+    cli = parsecli([assembly])
+    assert cli['<assembly>'] == assembly
+
+def test_main_with_assembly_and_output_file():
+    assembly = 'assembly.xml'
+    output = 'output.xml'
+    cli = parsecli([assembly, output])
+    assert cli['<assembly>'] == assembly
+    assert cli['<output>'] == output
