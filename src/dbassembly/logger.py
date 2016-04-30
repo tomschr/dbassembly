22c04326783fe614d407f2f47e52d654d0b7d8d3
Thomas Schraitle <tom_schr@web.de>
2016-04-30 15:26:23 +0200

    Add test case for logger.py

--- src/dbassembly/logger.py
+++ src/dbassembly/logger.py
@@ -3,18 +3,18 @@
 #
 # This file is part of dbassembly.
 #
-# kiwi is free software: you can redistribute it and/or modify
+# dbassembly is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# kiwi is distributed in the hope that it will be useful,
+# dbassembly is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with kiwi.  If not, see <http://www.gnu.org/licenses/>
+# along with dbassembly.  If not, see <http://www.gnu.org/licenses/>
 #
 import logging
 import sys
@@ -88,7 +88,7 @@ class ColorMessage(object):
             '$LIGHTCOLOR',
             self.esc['color_light'] % (30 + self.color[level])
         )
-        for color_name, color_id in list(self.color.items()):
+        for color_name, color_id in self.color.items():
             message = message.replace(
                 '$' + color_name,
                 self.esc['color'] % (color_id + 30)
@@ -327,7 +327,6 @@ class Logger(logging.Logger):
         self.console_handlers[handler_type] = handler
 
 
-
 def setloglevel(verbose):
     """Set log level according to verbose argument
 
@@ -348,3 +347,4 @@ log = logging.getLogger("dbasmy")
 
 # This is the default log level:
 log.setLevel(logging.DEBUG)
+log.set_color_format()
