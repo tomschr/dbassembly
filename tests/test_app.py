54ece343b94f8281674bbdb7146201d466fbe1cc
Thomas Schraitle <tom_schr@web.de>
2016-05-28 23:56:40 +0200

    New testcase: test_app_create_file

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,9 +11,17 @@ def test_app():
     app = App(None)
     assert repr(app)
 
+
 @raises(FileExistsError)
 @patch('dbassembly.app.os.path.exists')
 def test_app_file_exists(mock_exist):
-    # mock_open.side_effect = func_open
     mock_exist.return_value = True
     app = App({'<assembly>':'a', '--output':'b'})
+
+
+@patch('dbassembly.app.os.path.exists')
+@patch('builtins.open')
+def test_app_create_file(mock_open, mock_exist):
+    mock_exist.return_value = False
+    mock_open.return_value = StringIO()
+    app = App({'<assembly>':'a', '--output': mock_open})
