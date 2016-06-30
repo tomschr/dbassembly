709f42e024050c50abbc975aa87751bab2e74886
Thomas Schraitle <toms@suse.de>
2016-06-30 15:55:24 +0200

    Split test_dbassembly.py
    into test_cases.py and test_struct.py

---
+++ tests/test_cases.py
@@ -0,0 +1,53 @@
+#
+#
+from io import StringIO
+from unittest.mock import patch
+
+from lxml import etree
+from py.path import local
+from testfixtures import LogCapture
+
+from dbassembly.app import loadassembly
+from dbassembly.cli import main_app
+from dbassembly.assembly import assembly
+from dbassembly.exceptions import BaseAssemblyError
+from dbassembly.logger import log
+
+from .conftest import xmldump
+
+
+def test_cases(xmltestcase):
+    """Runs a testcase from cases/. This is used if you need an *exact*
+       comparison between real output and expected output.
+       The xmltestcase argument contains a tuple with three items (for example):
+
+       * local('foo.case.xml') is the assembly file
+       * local('foo.out.xml') is a (possible) output file; the content has to
+         be an *exact* output.
+       * local('foo.err.xml') is a (possible) file which contains errors from
+         logging.
+
+    :param xmltestcase: Contains tuple: (assembly, outputfile, errorfile)
+    :param tmpdir: temporary directory
+    :param capsys: fixture to capture sys.stdout & sys.stderr
+    """
+    assemblyfile, outputfile, errorfile = xmltestcase
+
+    with patch('dbassembly.app.sys.stdout', new=StringIO()) as mock_sys:
+        with LogCapture(names=(log.name,)) as ll:
+            result = main_app({'<assembly>': str(assemblyfile),
+                               '--output': None})
+
+    outputerr = errorfile.readlines(cr=False) if errorfile.isfile() else ['']
+    outputxml = outputfile.read().rstrip() if outputfile.isfile() else ""
+    # Check for empty strings at the end and remove them
+    if outputerr[-1] == '':
+        outputerr.pop()
+
+    logerror = []
+    for l in ll.records:
+        if l.levelname != 'ERROR': continue
+        logerror.extend(l.getMessage().rstrip().split("\n"))
+
+    assert outputerr == logerror
+    assert outputxml == mock_sys.getvalue()
--- tests/test_struct.py
+++ tests/test_struct.py
@@ -16,43 +16,6 @@ from dbassembly.logger import log
 from .conftest import xmldump
 
 
-def test_cases(xmltestcase):
-    """Runs a testcase from cases/. This is used if you need an *exact*
-       comparison between real output and expected output.
-       The xmltestcase argument contains a tuple with three items (for example):
-
-       * local('foo.case.xml') is the assembly file
-       * local('foo.out.xml') is a (possible) output file; the content has to
-         be an *exact* output.
-       * local('foo.err.xml') is a (possible) file which contains errors from
-         logging.
-
-    :param xmltestcase: Contains tuple: (assembly, outputfile, errorfile)
-    :param tmpdir: temporary directory
-    :param capsys: fixture to capture sys.stdout & sys.stderr
-    """
-    assemblyfile, outputfile, errorfile = xmltestcase
-
-    with patch('dbassembly.app.sys.stdout', new=StringIO()) as mock_sys:
-        with LogCapture(names=(log.name,)) as ll:
-            result = main_app({'<assembly>': str(assemblyfile),
-                               '--output': None})
-
-    outputerr = errorfile.readlines(cr=False) if errorfile.isfile() else ['']
-    outputxml = outputfile.read().rstrip() if outputfile.isfile() else ""
-    # Check for empty strings at the end and remove them
-    if outputerr[-1] == '':
-        outputerr.pop()
-
-    logerror = []
-    for l in ll.records:
-        if l.levelname != 'ERROR': continue
-        logerror.extend(l.getMessage().rstrip().split("\n"))
-
-    assert outputerr == logerror
-    assert outputxml == mock_sys.getvalue()
-
-
 def test_structures(xmlteststruct):
     """Runs a testcase from struct/. This is used if you are more interested in
        the structure than the real representation of the XML.
@@ -92,5 +55,3 @@ def test_structures(xmlteststruct):
 
     assert outputerr == logerror
     assert outputstruct == resultlist
-
-
